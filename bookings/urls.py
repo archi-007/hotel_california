df9873c0ffc8a1fca878d5b805ec7db138442243
archi-007 <archiribhu@gmail.com>
2020-03-25 22:29:15 +0530

    modified the bookings section

--- bookings/urls.py
+++ bookings/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('room_book/', views.bookaroom, name='bookaroom'),
     path('booking_successful/', views.successbook, name='successbook'),
     path('bookfood/', views.bookatable, name = 'bookatable'),
-    
+    path('pastbookingsroom/', views.viewroombooking, name = 'userroombook'),
+    path('pastbookingstable/', views.viewtablebooking, name = 'usertablebook'),
     
     ]
--- bookings/views.py
+++ bookings/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 
-
+from .models import Book_a_room, Book_a_table
 from .forms import Room_book, Table_book
 
 # Create your views here.
@@ -13,25 +13,48 @@ from .forms import Room_book, Table_book
 def successbook(request):
     return render(request, 'booking_successful.html')
 
+@login_required
+def viewroombooking(request):
+    bookroom = Book_a_room.objects.all()
+    context = {
+        'bookroom' : bookroom
+    }
+    return render(request, 'viewpastbookingsroom.html', context)
+
+
+
+@login_required
+def viewtablebooking(request):
+    booktable = Book_a_table.objects.all()
+    context = {
+        'booktable' : booktable
+    }
+    return render(request, 'viewpastbookingstable.html', context)
+
 
 
 
 
 @login_required
 def bookaroom(request):
+
+    
+
     context = {}
     form = Room_book()
     if (request.method == "POST"):
         form = Room_book(request.POST)
         
         if (form.is_valid()):
-            form.save()
+            book = form.save(commit=False)
+            book.user = request.user
+            book.save()
             return redirect('/booking_successful')
 
 
     return render(request, 'room_book.html', {'form' : form})
 
-
+ 
 
 
 
@@ -43,7 +66,9 @@ def bookatable(request):
         form = Table_book(request.POST)
         
         if (form.is_valid()):
-            form.save()
+            book = form.save(commit=False)
+            book.user = request.user
+            book.save()
             return redirect('/booking_successful')
 
 
